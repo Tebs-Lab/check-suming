13b88a94b12384f352de0139a355a0e758bdbd8b
Tyler Bettilyon <teb311@gmail.com>
2016-11-19 15:27:34 -0800

    added pending tests

---
+++ src/tests/corrut-string.js
@@ -0,0 +1,22 @@
+'use strict'
+const corrupt = require('../corrupt-string');
+const assert = require('chai').assert
+
+describe("corrupt-string", function() {
+
+  xdescribe("shuffleString", function() {
+
+  });
+
+  xdescribe("corruptBlock", function() {
+
+  });
+
+  xdescribe("swapCharacters", function() {
+
+  });
+
+  xdescribe("bitShiftOneCharacter", function() {
+
+  });
+});
