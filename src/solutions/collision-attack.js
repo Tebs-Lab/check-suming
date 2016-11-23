40a0b634135530327c5dee8daaf00c912d163087
Tyler Bettilyon <teb311@gmail.com>
2016-11-23 15:24:51 -0700

    delete a bunch of stuff for a better life

--- src/solutions/collision-attack.js
+++ src/solutions/collision-attack.js
@@ -39,6 +39,8 @@ module.exports = {
 
     @param {integer} checksum : the hash value that our returned string must
                                 hash to using the simple sum method
+
+    @param {string} characterSet : A string with at least one of each allowed character
   */
   collideWithSimpleSumRestricted: function(checksum, characterSet) {
     // Performance optimization, greedily pick bigger charCodes first
@@ -74,195 +76,41 @@ module.exports = {
     return a value that collides with the provided checksum when
     the charcodeSum function is used to hash the return value.
 
-    Once again, we're restricted to a specific character set.
+    Once again, we're restricted to a specific character set. Unfortnuately,
+    this checksum can't realistically be broken with a brute force search. Doing
+    so requires solving the "subset-sum" problem, which is NP-Complete. Using a
+    similar strategy to the above becomes quite slow as soon as we need 16-32 digit
+    strings.
+
+    Instead we're [doing something that takes advantage of the checksum].
 
     @param {integer} checksum : the hash value that our returned string must
                                 hash to using the simple sum method
+
+    @param {string} characterSet : A string with at least one of each allowed character
   */
   collideWithIndex: function(checksum, characterSet) {
 
-    const numbers = createIndexTimesNumberMap(checksum, characterSet);
+    let [maxLength, numbers] = createIndexTimesNumberMap(checksum, characterSet);
 
     let charSetOrdered = characterSet.split('').sort(function(a, b) {
         return a.charCodeAt(0) - b.charCodeAt(0);
     });
-    let maxCharCode = charSetOrdered[0].charCodeAt(0);
-    let fourDigitsOfMax = 10 * maxCharCode;
+    let minCharCode = charSetOrdered[0].charCodeAt(0);
 
-    // Instead of exhaustively searching, we're "padding" until we've reduced our
-    // actual search space to about 8 digits.
-    let searchSum = checksum;
-    let mockStaringIndex = 9;
-    while(searchSum > fourDigitsOfMax) {
-      searchSum -= mockStaringIndex * maxCharCode;
-      mockStaringIndex += 1;
+    let startingPoint = 0;
+    let valueAtPosition = [];
+    for(let i = 1; i < maxLength; i++) {
+      let value = i * minCharCode;
+      valueAtPosition[i-1] = value;
+      startingPoint += value;
     }
 
-    // "unpad" if we take it too far
-    if(searchSum < 0) {
-      searchSum += mockStaringIndex * maxCharCode;
-      mockStaringIndex == 1;
-    }
+    let offBy = checksum - startingPoint;
+    console.log(valueAtPosition);
 
-    console.log(searchSum, checksum, mockStaringIndex, fourDigitsOfMax);
-    let collisionObj = generateAllSums(searchSum, 0);
-    console.log(collisionObj[0], collisionObj[0] + charSetOrdered[0].repeat(mockStaringIndex - 9));
-    return collisionObj[0] + charSetOrdered[0].repeat(mockStaringIndex - 9);
-
-    /**
-      Now we're using brute force again, but finding the numbers that
-      sum to our checksum instead of searching for strings that break
-      the checksum. This problem is actually well studied, and called
-      "The subset sum problem". It's also "NP-Complete", which means
-      many things, but also means we won't be able to generate all
-      such sums. So we have to limit the search space.
-
-      Luckily, we have some very practical limits in the position
-      and character set. Each option in the numberMap can only
-      be used once, for example, since they each represent a
-      character at an individual position in the string.
-    */
-    function generateAllSums(checksum, currentSum, partial = [], usedInfo = undefined) {
-      // Default for the usedInfo is complex
-      if(usedInfo === undefined) {
-        usedInfo = {
-          numbers: {},
-          positions: {}
-        };
-      }
-
-      if(currentSum > checksum) return [];
-
-      if(currentSum === checksum){
-        let collisions = computePotentialCollisions(numbers, partial);
-        if(collisions === false) {
-          return [];
-        }
-
-        for(let c of collisions) {
-          if(checksums.charcodeTimesIndex(c) === checksum) {
-            return [c]
-          }
-        }
-      }
-
-      // Performance: Compute the numbers we need to work on BEFORE we clone the info
-      let validNumbers = Object.keys(numbers).filter(function(number) {
-
-        let openPosition = false;
-        for(let {position} of numbers[number]) {
-          // console.log(position, usedInfo.positions[position]);
-          if(!usedInfo.positions[position]) {
-            openPosition = true;
-            break;
-          }
-        }
-
-        return openPosition && (!usedInfo.numbers[number] || usedInfo.numbers[number] < numbers[number].length);
-      });
-
-      let sumsFoundBelow = [];
-      for(let number of validNumbers) {
-        let newUsedInfo = cloneUsedInfo(usedInfo);
-        let usedNumbers = newUsedInfo.numbers;
-        let usedPositions = newUsedInfo.positions;
-
-        // Increment this number's used count
-        if(usedNumbers[number] === undefined) usedNumbers[number] = 1;
-        else usedNumbers[number] += 1;
-
-        // Mark one of the possible positions at this location as used
-        let n = numbers[number];
-        for(let {position} of n) {
-
-          if(!usedPositions[position]) {
-            usedPositions[position] = true;
-            break;
-          }
-        }
-
-        let newSum = currentSum + Number(number);
-        let newCollision = partial.slice();
-        newCollision.push(number);
-
-        sumsFoundBelow = sumsFoundBelow.concat(generateAllSums(checksum, newSum, newCollision, newUsedInfo));
-        if(sumsFoundBelow.length > 0) {
-          return sumsFoundBelow;
-        }
-      }
-
-      return sumsFoundBelow;
-    }
+    console.log(startingPoint, checksum, offBy);
   }
-}
-
-
-// Quick way to make a deep clone of the usedInfo obj
-function cloneUsedInfo(usedInfo) {
-  let newObj = {
-    numbers: {},
-    positions: {}
-  };
-
-  for(let number in usedInfo.numbers) {
-    newObj.numbers[number] = usedInfo.numbers[number];
-  }
-
-  for(let position in usedInfo.positions) {
-    newObj.positions[position] = usedInfo.positions[position];
-  }
-
-  return newObj;
-}
-
-/**
-  Given a number map, and the subset sum solution, see if we can use this subset
-  as our collision given the constraints of the numberMap. We use another exhaustive
-  recursive search to generate all the strings represented.
-*/
-function computePotentialCollisions(numberMap, partial, partialCollision = []) {
-  let positionCharacterMap = {}
-  for(let number of partial) {
-    let options = numberMap[number];
-    for(let option of options) {
-      if(positionCharacterMap[option.position] === undefined) {
-        positionCharacterMap[option.position] = [];
-      }
-      positionCharacterMap[option.position].push(option.character);
-    }
-  }
-
-  // Check that it's possible
-  for(let i = 0; i < partial.length; i++) {
-    if(positionCharacterMap[i] === undefined) {
-      return false;
-    }
-  }
-
-  return stringsFromPositionMap(positionCharacterMap);
-}
-
-function stringsFromPositionMap(positionCharacterMap, position = 0, prefix = '') {
-  let stringsBelow = [];
-  if(prefix.length >= Object.keys(positionCharacterMap).length){
-    stringsBelow.push(prefix);
-    return stringsBelow;
-  }
-
-  for(let i = position; i < Object.keys(positionCharacterMap).length; i++) {
-    let characters = positionCharacterMap[i];
-
-    // Impossible -- don't continue, no possible strings below.
-    if(!characters) {
-      return [];
-    }
-
-    for(let char of characters) {
-      stringsBelow = stringsBelow.concat(stringsFromPositionMap(positionCharacterMap, i+1, prefix + char));
-    }
-  }
-
-  return stringsBelow;
 }
 
 /**
@@ -300,5 +148,5 @@ function createIndexTimesNumberMap(checksum, characteSet) {
     }
   }
 
-  return numbers;
+  return [maxLength, numbers];
 }
